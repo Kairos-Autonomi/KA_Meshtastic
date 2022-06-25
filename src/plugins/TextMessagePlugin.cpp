551c0356a6b98bda4603f2e76c06a8abbfdde5d4
alexCharters <alexcharters45@gmail.com>
2022-06-25 12:34:57 -0700

    cleaned commented code blocks

--- src/plugins/TextMessagePlugin.cpp
+++ src/plugins/TextMessagePlugin.cpp
@@ -20,36 +20,12 @@ ProcessMessage TextMessagePlugin::handleReceived(const MeshPacket &mp)
     if(strcmp(cmd, "linkReq") == 0){
         triggerPlugin->linkReqd = true;
         triggerPlugin->reqd_from = mp.from;
-        // strcpy((char*)mp.decoded.payload.bytes, sarg1);
-        // Serial.println(sarg1);
-        // if(triggerPlugin->isAttemptingLink){
-        //     if(triggerPlugin->isLinked){
-        //         ignore = true;
-        //     }
-        //     else{
-        //         // char buffer [256];
-        //         // char buffer2 [12];
-        //         const unsigned int from = mp.from;
-                
-        //         // strcat(buffer, "linkConf ");
-        //         // strcat(buffer, utoa(from,buffer2,10));
-        //         // strcat(buffer, " ");
-        //         // strcat(buffer, utoa(nodeDB.getNodeNum(),buffer2,10));
-        //         // DEBUG_MSG("Sending msg=%.*s\n", buffer);
-        //         cannedMessagePlugin->sendText(from, "linkConf", true);
-        //     }
-        // }
     }
     else if(strcmp(cmd, "linkAck") == 0){
         triggerPlugin->isLinked = true;
         triggerPlugin->linked_id = mp.from;
         cannedMessagePlugin->sendText(mp.from, "linkConf", false);
         cannedMessagePlugin->sendText(NODENUM_BROADCAST, "reqComp", false);
-        // if(!triggerPlugin->isLinked){
-        //     triggerPlugin->isLinked = true;
-        //     triggerPlugin->linked_id = mp.from;
-        //     cannedMessagePlugin->sendText(mp.from, "linkConf", true);
-        // }
     }
     else if(strcmp(cmd, "linkConf") == 0){
         triggerPlugin->isLinked = true;
@@ -62,21 +38,6 @@ ProcessMessage TextMessagePlugin::handleReceived(const MeshPacket &mp)
         const int len = strlen((char*)p.payload.bytes);
         const int txBytes = uart_write_bytes(UART_NUM_2, "trigger fire\n\r", len);
     }
-    // else{
-    //     char buffer[12];
-    //     char* cmd = strtok((char*)p.payload.bytes, " ");
-    //     char* responded_to = strtok((char*)p.payload.bytes, " ");
-    //     char* response_from = strtok((char*)p.payload.bytes, " ");
-    //     char* my_number = utoa(nodeDB.getNodeNum(),buffer,10);
-    //     if(strcmp(cmd, "linkConf") == 0 && strcmp(responded_to, my_number) == 0){
-    //         triggerPlugin->hasReceivedLinkConf = true;
-    //         triggerPlugin->isLinked = true;
-    //         triggerPlugin->linked_id=atoi(response_from);
-    //         DEBUG_MSG("Recieved conf. Linked.");
-    //     }else{
-    //         DEBUG_MSG("Recieved conf. Not Linked.");
-    //     }
-    //}
 
     // We only store/display messages destined for us.
     // Keep a copy of the most recent text message.
